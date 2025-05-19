daa78b956879c56c0ca4209cab4b49b43ec74131
Malaming <minggggggg@gmail.com>
2025-05-20 00:46:20 +0700

    feat: add style dictionary to and design tokens file all.json for improve development

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-24 font-family-poppins  ">
+      <p className=" "></p>
+        Welcome to <a href="https://nextjs.org">Plasom</a>
+
+        <p className="text-button-s font-bold ">Hello Tailwind 4</p>
+    </div>
+  );
+}
