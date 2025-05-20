f023cf58afc22e5ceee1df03adcfc38a4e918491
Malaming <minggggggg@gmail.com>
2025-05-20 12:55:47 +0700

    feat: update global styles and component classes for improved design consistency

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-24 font-family-poppins  ">
+    <div className="flex flex-col items-center justify-center min-h-screen p-24 font-family-poppins rounded-eueu font-heading-h3">
       <p className=" "></p>
         Welcome to <a href="https://nextjs.org">Plasom</a>
 
-        <p className="text-button-s font-bold ">Hello Tailwind 4</p>
+        <p className="text-button-s font-bold text-[var(--semantic-color-brand-secondary)] ">Hello Tailwind 4</p>
     </div>
   );
 }
